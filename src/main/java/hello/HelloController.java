3c1e3d40ab96e7fd9d0e99d499805925b5e676d2
Jason <840495823@qq.com>
2020-03-02 15:10:28 +0800

    v1.1

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,9 +2,18 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import hello.service.OrderService;
+
+import javax.inject.Inject;
 
 @RestController
 public class HelloController {
+    private OrderService orderservice;
+
+    @Inject
+    public HelloController(OrderService orderservice) {
+        this.orderservice = orderservice;
+    }
 
     @RequestMapping("/")
     public String index() {
