12d79e81aea03b68c8dd8ce78e3b5c565b10edff
Jason <840495823@qq.com>
2020-03-12 05:39:56 +0800

    v1.7

--- src/main/java/hello/configuration/JavaConfiguration.java
+++ src/main/java/hello/configuration/JavaConfiguration.java
@@ -2,7 +2,7 @@ package hello.configuration;
 
 import org.springframework.context.annotation.Configuration;
 
-//It just a test
+//It just a test for v1.7(travis ci)
 @Configuration
 public class JavaConfiguration {
 }
